8220398c21d30881e589f0809693fd5b61acf75a
trung71 <nmtrung@ctu.edu.vn>
2026-01-09 16:07:45 +0700

    First version

---
+++ test.js
@@ -0,0 +1,9 @@
+const persons = [
+  {firstname : "Malcom", lastname: "Reynolds"},
+  {firstname : "Kaylee", lastname: "Frye"},
+  {firstname : "Jayne", lastname: "Cobb"}
+];
+
+console.log(persons.map((p) => {
+  return [p.firstname, p.lastname].join(" ");
+}));
